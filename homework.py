6bcb152da9f97859bdb12b3e33e41d159f345a6a
Tatiana <tanchaz@ya.ru>
2021-02-02 22:51:42 +0300

    before deploy

--- homework.py
+++ homework.py
@@ -1,9 +1,17 @@
+import logging
 import os
 import time
 
 import requests
-import telegram
 from dotenv import load_dotenv
+from telegram import Bot
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='my_logger.log',
+    filemode='w',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
 load_dotenv()
 
@@ -11,43 +19,80 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
+    try:
+        homework_name = homework['homework_name']
+    except KeyError:
+        logging.error(KeyError, exc_info=True)
+        return f'Бот столкнулся с ошибкой: {KeyError}'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-
-
-def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
-    return homework_statuses.json()
+        try:
+            homework_status = homework['status']
+        except KeyError:
+            logging.error(KeyError, exc_info=True)
+            return f'Бот столкнулся с ошибкой: {KeyError}'
+        else:
+            if homework_status == 'rejected':
+                verdict = 'К сожалению в работе нашлись ошибки.'
+            else:
+                verdict = (
+                    'Ревьюеру всё понравилось, '
+                    'можно приступать к следующему уроку.'
+                )
+            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+    return bot_client.send_message(chat_id=CHAT_ID, text=message)
+
+
+def get_homework_statuses(current_timestamp):
+    headers = {
+        'Authorization': f'OAuth {PRAKTIKUM_TOKEN}',
+    }
+    params = {
+        'from_date': current_timestamp,
+    }
+    try:
+        response = requests.get(API_URL, params=params, headers=headers)
+    except Exception as error:
+        logging.error(error, exc_info=True)
+        return f'Бот столкнулся с ошибкой: {error}'
+    else:
+        homework_statuses = response
+        return homework_statuses.json()
 
 
 def main():
-    # проинициализировать бота здесь
+    bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())  # начальное значение timestamp
-
     while True:
+        homework = get_homework_statuses(current_timestamp)
         try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
-
-        except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            homeworks_list = homework['homeworks']
+        except TypeError:
+            send_message(f'Бот столкнулся с ошибкой: {homework}', bot)
+        except KeyError:
+            logging.error(KeyError, exc_info=True)
+            send_message(f'Бот столкнулся с ошибкой: {KeyError}', bot)
+        else:
+            try:
+                new_homework = homeworks_list[0]
+            except IndexError:
+                logging.error(IndexError, exc_info=True)
+                send_message(f'Бот столкнулся с ошибкой: {IndexError}', bot)
+            else:
+                send_message(parse_homework_status(new_homework), bot)
+            finally:
+                current_timestamp = homework.get(
+                    'current_date',
+                    current_timestamp
+                )
+        finally:
+            time.sleep(300)
 
 
 if __name__ == '__main__':
